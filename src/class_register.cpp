91ff670f476feade40f2877b7657d043374c7a5c
Codecat <spansjh@gmail.com>
2018-12-24 16:48:08 +0100

    Implicit upcast

--- src/class_register.cpp
+++ src/class_register.cpp
@@ -2,6 +2,8 @@
 
 std::vector<CClassRegister*> ClassRegister::g_instances;
 
+static void* ScriptUpcast(void* p) { return p; }
+
 CClassRegister::CClassRegister(asIScriptEngine* engine, const char* name, int byteSize, asDWORD flags, const char* parent)
 {
 	m_engine = engine;
@@ -100,7 +102,10 @@ void CClassRegister::RegisterChild(CClassRegister* child)
 		m_engine->RegisterObjectBehaviour(child->m_name.c_str(), behavior.m_behavior, behavior.m_declaration.c_str(), behavior.m_funcPointer, behavior.m_callConv, behavior.m_auxiliary, behavior.m_compositeOffset, behavior.m_isCompositeIndirect);
 	}
 
-	//TODO: Implicit upcast and explicit downcast functions
+	std::string declUpcast = m_name + "@ opImplCast()";
+	m_engine->RegisterObjectMethod(child->m_name.c_str(), declUpcast.c_str(), asFUNCTION(ScriptUpcast), asCALL_CDECL_OBJFIRST);
+
+	//TODO: Explicit downcast
 }
 
 CClassRegister* ClassRegister::New(asIScriptEngine* engine, const char* name, int byteSize, asDWORD flags, const char* parent)
