7dc4f1ada1a3baa90a43d5f28701d44dc153a7a9
Daniel Liljeberg <daniel@liljeberg.xyz>
2018-12-27 21:41:00 +0100

    for glm just use include

--- cmake/FindGLM.cmake
+++ cmake/FindGLM.cmake
@@ -5,15 +5,16 @@
 #  GLM_LIBRARY
 
 set(GLM_SEARCH_PATHS
-	/usr/local/include
-	/usr/include
+	/usr/local
+	/usr
 	)
 
 find_path(GLM_INCLUDE_DIR
 	NAMES glm.hpp
-	PATH_SUFFIXES glm
+	PATH_SUFFIXES include include/glm
 	PATHS ${GLM_SEARCH_PATHS})
 
+string(REPLACE "/glm" "" GLM_INCLUDE_DIR "${GLM_INCLUDE_DIR}")
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLM DEFAULT_MSG GLM_INCLUDE_DIR)
 
